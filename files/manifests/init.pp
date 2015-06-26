c68aae8e7c2b47e46023782afbf1b6339e7ea18a
Boonchu Ngampairoijpibul <bngampairoijpibul@machinezone.com>
2015-06-26 16:34:57 -0700

    updated manifests

--- files/manifests/init.pp
+++ files/manifests/init.pp
@@ -1,39 +1,16 @@
 # == Class: files
 #
-# Full description of class files here.
+# files - contains files
 #
 # === Parameters
 #
-# Document parameters here.
+# [*say_hello_to*]
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { files:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
+# [*myname*]
 #
 # === Authors
 #
-# Author Name puppet@example.com
-#
-# === Copyright
-#
-# Copyright (C) 2015 Example, Inc.
+# Author Name bigchoo@gmail.com
 #
 class files (
     $say_hello_to = $files::params::say_hello_to,
---
+++ files/manifests/params.pp
@@ -0,0 +1,7 @@
+# Class contains parameter value
+class files::params {
+
+    $say_hello_to = 'mickey_mouse'
+    $myname       = 'file03'
+
+}
