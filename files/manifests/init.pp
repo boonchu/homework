7865856c0759a384268a662ce2bedf4d830955a6
Boonchu Ngampairoijpibul <bngampairoijpibul@machinezone.com>
2015-06-29 19:04:26 -0700

    updated bn/use_parser_future

--- files/manifests/init.pp
+++ files/manifests/init.pp
@@ -13,13 +13,39 @@
 # Author Name bigchoo@gmail.com
 #
 class files (
+
     $say_hello_to = $files::params::say_hello_to,
     $myname       = $files::params::myname,
+
+    $instances    = $files::params::instances,
+    $owner        = $files::params::owner,
+    $group        = $files::params::group,
+
 ) inherits files::params {
 
     file { "/tmp/${myname}":
         ensure  => file,
         content => template('files/probeme.erb')
+    }
+
+    $n = range('1', $::files::instances)
+    if $n {
+      validate_array($n)
+    }
+
+    notify {"[DEBUG] setup multiple files :: ${n}": withpath => true}
+
+    each($n) |$instance| {
+
+      $counter = 30 + $instance
+
+      file { "/var/log/files-16${counter}":
+        ensure => 'directory',
+        owner  => $owner,
+        group  => $group,
+        mode   => '0555',
+      }
+
     }
 
 }
--- files/manifests/params.pp
+++ files/manifests/params.pp
@@ -3,5 +3,8 @@ class files::params {
 
     $say_hello_to = 'mickey_mouse'
     $myname       = 'file03'
+    $instances     = '5'
+    $owner        = 'root'
+    $group        = 'root'
 
 }
